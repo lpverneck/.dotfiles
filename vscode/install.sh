60e4b112ab3c9a4360bfc314b4cc8338a861183f
Lucas Verneck <49991581+lpverneck@users.noreply.github.com>
2022-08-28 15:48:01 -0300

    Add file for extensions install

---
+++ vscode/install.sh
@@ -0,0 +1,5 @@
+code --install-extension teabyii.ayu
+code --install-extension ms-python.python
+code --install-extension njpwerner.autodocstring
+code --install-extension VisualStudioExptTeam.vscodeintellicode
+code --install-extension eamodio.gitlens
